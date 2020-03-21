1ed3ac58764a92b7bd3d10f4b411c3fa93362755
caochanhduong <caochanhduongbku@gmail.com>
2020-03-21 12:05:58 +0700

    delete library

--- .ipynb_checkpoints/message_handler-checkpoint.py
+++ .ipynb_checkpoints/message_handler-checkpoint.py
@@ -8,7 +8,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from fastai.text import *
 import torch
 import pickle
-import pandas as pd
 import numpy as np
 from sklearn.externals import joblib
 from sklearn import preprocessing
